dcb7455c712139f1fd4f19fa63d329f6403a6924
bernispitzwieser <bernhard.spitzwieser@gmx.at>
2026-06-11 07:20:57 +0200

    Add files via upload

---
+++ img/auftritte.js
@@ -0,0 +1,32 @@
+// BRASSDORFER AUFTRITTE
+// Hier kannst du eure Termine zentral bearbeiten.
+// Nach dem Speichern übernehmen Startseite und Events-Seite die Termine automatisch.
+// Wichtig: Datum im Format JJJJ-MM-TT schreiben, z. B. "2026-07-03".
+// Ort und Info werden beim Aufklappen eines Termins angezeigt.
+
+window.BRASSDORFER_AUFTRITTE = [
+  {
+    datum: "2026-06-12",
+    titel: "Musikfest Moosdorf",
+    ort: "AT-5141 Moosdorf, Oberösterreich",
+    info: ""
+  },
+  {
+    datum: "2026-07-03",
+    titel: "Waldfest Lamprechtshausen",
+    ort: "AT-5112 Lamprechtshausen, Salzburg",
+    info: ""
+  },
+  {
+    datum: "2026-09-06",
+    titel: "Kornmandlfest Lochen",
+    ort: "AT-5221 Lochen am See, Oberösterreich",
+    info: ""
+  },
+  {
+    datum: "2026-09-11",
+    titel: "FF-Fest Hochburg-Ach",
+    ort: "AT-5122 Hochburg-Ach, Oberösterreich",
+    info: ""
+  }
+];
